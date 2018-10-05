2379c1d62ff74a70025f0c026b1c76aef62f5c68
InexorableTides <TheTideIsInfinite@gmail.com>
2018-10-04 19:52:01 -0500

    OkOkOkOkOkDONE

--- main.py
+++ main.py
@@ -26,29 +26,43 @@ def validation():
     email_regex = re.compile(r"[^@\s]+@[^@\s]+\.[a-zA-Z0-9]+$")
 
     if len(name) < 3:
-        name_error = "The username is too short"
+        name_error = "The username is too short(between 3-20 characters)"
         return render_template("base.html", your_name=name, your_email=email, name_error=name_error)
     if len(name) > 20:
-        name_error = "The username is too long"
+        name_error = "The username is too long(between 3-20 characters)"
         return render_template("base.html", your_name=name, your_email=email, name_error=name_error)
     if name.isalnum() and password == repeated and len(email) > 20:
-        mail_error = "E-Mail is too long!"
-        return render_template("base.html", your_name=name, your_email=email, email_error=mail_error)
+        email_error = "E-Mail is too long!(between 3-20 characters)"
+        return render_template("base.html", your_name=name, your_email=email, email_error=email_error)
+    if name.isalnum() and password == repeated and len(password) > 20:
+        password_error = "Password is too long!(between 3 and 20 Characters)"
+        return render_template("base.html", your_name=name, your_email=email, password_error=password_error )
+    if name.isalnum() and password == repeated and len(password) < 3:
+        password_error = "Password is too short!(between 3 and 20 Characters)"
+        return render_template("base.html", your_name=name, your_email=email, password_error=password_error)     
+    if name.isalnum() and password == repeated and len(repeated) > 20:
+        password_error = "Password is too long!(between 3 and 20 Characters)"
+        return render_template("base.html", your_name=name, your_email=email ,password_error=password_error )
+    if name.isalnum() and password == repeated and len(repeated) < 3:
+        password_error = "Password is too short!(between 3 and 20 Characters)"
+        return render_template("base.html", your_name=name, your_email=email, password_error=password_error)     
+     
+    
     if name.isalnum() and password == repeated and len(email) < 3:
-        mail_error = "E-mail is too short!"
-        return render_template("base.html", your_name=name, your_email=email, email_error=mail_error)     
+        email_error = "E-mail is too short!(between 3-20 characters)"
+        return render_template("base.html", your_name=name, your_email=email, email_error=email_error)     
     if name.isalnum() and password == repeated and len(email) :
         if not email_regex.match(email): 
-            mail_error = "This is not a valid E-mail!"
-            return render_template("base.html", your_name=name, your_email=email, email_error=mail_error)
+            email_error = "This is not a valid E-mail!"
+            return render_template("base.html", your_name=name, your_email=email, email_error=email_error)
         if email_regex.match(email):
             return render_template("sucess.html", your_name=name)
             
     if name.isalnum() and password != repeated:
-        password_error = "The passwords are invalid"
+        password_error = "The passwords do not match!"
         return render_template("base.html", your_name=name, your_email=email, password_error=password_error)
     else:
-        name_error = "The username is invalid"
+        name_error = "The username is invalid(cannot contain spaces or non alphanumerics)"
         return render_template("base.html", your_name=name, your_email=email, name_error=name_error)
 
 app.run()
